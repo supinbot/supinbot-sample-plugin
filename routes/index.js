8ded71dd97e7e71be5ebd66614edf3fbbb6791e5
Starfox64 <louisdijon21@yahoo.fr>
2016-07-14 11:37:45 -0400

    Fixed templating issue

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
-	res.render('sample/sample');
+	res.render('sample/sample.html');
 });
 
 module.exports = router;
